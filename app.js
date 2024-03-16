07f5e0368592332061c847beee7cf50e089587b0
MindeF MindeL <mindegi15@gmail.com>
2024-03-16 10:57:55 +0200

    implemented Portfolio tab

--- app.js
+++ app.js
@@ -19,16 +19,16 @@ function PageTransitions() {
             sectBtns.forEach((btn) => {
                 btn.classList.remove('active');
             });
+            
+            e.target.classList.add('active');
+    
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            });
+    
+            const element = document.getElementById(id);
+            element.classList.add('active');
         };
-
-        e.target.classList.add('active');
-
-        sections.forEach((section) => {
-            section.classList.remove('active');
-        });
-
-        const element = document.getElementById(id);
-        element.classList.add('active');
     });
 };
 
